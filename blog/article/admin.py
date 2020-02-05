ee0d1939c5f25f92ec1c771b1f9476e264336f28
jingyi <jingyi@jingyi-ASUS-Laptop-X560UD>
2020-02-05 23:03:50 +0800

    Chapter 8 finished

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article, Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['article', 'content']
+    list_display_links = ['article']
+    list_filter = ['article', 'content','pubDateTime']
+    search_fields = ['content']
+    list_editable = ['content']
+ 
+    class Meta:
+        model = Comment
+
+
+admin.site.register(Article)
+admin.site.register(Comment, CommentAdmin)
